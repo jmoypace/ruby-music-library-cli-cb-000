f0ba049339679243a8dfe7ec626eab997a1e5122
jmoypace <jmoypace@gmail.com>
2018-07-25 17:23:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,13 +24,15 @@ def self.create(name)
 artist=new(name)
 artist.save
 artist
-
 end
+
 def add_song(song)
     song.artist = self unless song.artist
     songs << song unless songs.include?(song)
-  end
-  def genres
+end
+
+def genres
     songs.collect{ |s| s.genre }.uniq
-  end
+end
+
 end
