a346ca9579bd9c1e7fe389bd3f5b646243bba88a
jmoypace <jmoypace@gmail.com>
2018-07-25 17:38:32 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -5,6 +5,12 @@ class MusicImporter
     @path = path
   end
 
-
+  def files
+      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    end
+  
+    def import
+      files.each{ |f| Song.create_from_filename(f) }
+    end
 
 end
