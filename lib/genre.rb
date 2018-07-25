052812214aaecf922b4b14af1e3dd7cb7e38a48a
jmoypace <jmoypace@gmail.com>
2018-07-25 17:25:28 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -7,6 +7,15 @@ def initialize(name)
   @name=name
   @songs = []
 end
+def self.create(name)
+genre=new(name)
+genre.save
+genre
+end
+
+def artists
+    songs.collect{ |s| s.artist }.uniq
+end
 def self.destroy_all
   @@all.clear
 end
@@ -18,13 +27,5 @@ def save
 self.class.all<<self
 end
 
-def self.create(name)
-genre=new(name)
-genre.save
-genre
 
-end
-def artists
-    songs.collect{ |s| s.artist }.uniq
-  end
 end
