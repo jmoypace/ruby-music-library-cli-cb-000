96d0518d56094f78c76d2845e8fe0d075949138d
jmoypace <jmoypace@gmail.com>
2018-07-25 17:14:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,10 @@ class Song
   attr_accessor :name
   attr_reader :artist
   @@all=[]
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
       @name = name
       self.artist = artist if artist
+      self.genre = genre if genre
     end
 
     def artist=(artist)
