684521740c351f078f365d8c7e3119cb92be951e
jmoypace <jmoypace@gmail.com>
2018-07-25 17:52:29 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,4 +44,31 @@ class MusicLibraryController
      puts "#{i}. #{a.name}"
    end
  end
+ def list_genres
+   Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+     puts "#{i}. #{g.name}"
+   end
+ end
+
+ def list_songs_by_artist
+   puts "Please enter the name of an artist:"
+   input = gets.strip
+
+   if artist = Artist.find_by_name(input)
+     artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+       puts "#{i}. #{s.name} - #{s.genre.name}"
+     end
+   end
+ end
+
+ def list_songs_by_genre
+   puts "Please enter the name of a genre:"
+   input = gets.strip
+
+   if genre = Genre.find_by_name(input)
+     genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+       puts "#{i}. #{s.artist.name} - #{s.name}"
+     end
+   end
+ end
 end
