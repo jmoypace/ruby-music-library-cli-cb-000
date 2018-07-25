924516d0bf41c4084f383a740e51488c2cd6ea76
jmoypace <jmoypace@gmail.com>
2018-07-25 17:12:33 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist
+  @@all=[]
+  def initialize(name, artist = nil)
+      @name = name
+      self.artist = artist if artist
+    end
+
+    def artist=(artist)
+        @artist = artist
+        artist.add_song(self)
+      end
+
+def self.destroy_all
+  @@all.clear
+end
+def self.all
+
+@@all
+end
+def save
+self.class.all<<self
+end
+def self.create(name)
+song=new(name)
+song.save
+song
+
+end
+end
